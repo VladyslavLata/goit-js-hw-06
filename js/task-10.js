cc07fc9cd1aabd1e9f74fde501a308081a1a1519
VladyslavLata <vladlatahtc@gmail.com>
2022-05-29 14:29:06 +0300

    10 task recompleted

--- js/task-10.js
+++ js/task-10.js
@@ -2,18 +2,23 @@ function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215).toString(16).padStart(6, 0)}`;
 }
 
-const inputEl = document.querySelector('input');
+const inputEl = document.querySelector('#controls input');
 const btnCreateEl = document.querySelector('[data-create]');
 const btnDestroyEl = document.querySelector('[data-destroy]');
 const boxesEl = document.getElementById('boxes');
 const boxEl = boxesEl.children;
 
-btnCreateEl.addEventListener('click', createBoxes);
+btnCreateEl.addEventListener('click', onClickBtnCreateBoxes);
 btnDestroyEl.addEventListener('click', onClickBtnCleanBoxes);
 
-function createBoxes() {
+function onClickBtnCreateBoxes() {
+  const currentValue = inputEl.value;
+  createBoxes(currentValue);
+}
+
+function createBoxes(amount) {
   let arrayBoxes = [];
-  for (let i = 0; i < inputEl.value; i += 1) {
+  for (let i = 0; i < amount; i += 1) {
     arrayBoxes.push(document.createElement('div'));
   }
 
